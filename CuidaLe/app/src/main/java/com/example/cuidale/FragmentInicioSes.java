61244f3dd3cd4a50c4b860d8f7698aa6295b8671
machete20 <sdj0002@alu.medac.es>
2025-01-28 12:41:57 +0100

    nav 0.2

--- CuidaLe/app/src/main/java/com/example/cuidale/FragmentInicioSes.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/FragmentInicioSes.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragPantInicio#newInstance} factory method to
+ * Use the {@link FragmentInicioSes#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragPantInicio extends Fragment {
+public class FragmentInicioSes extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class FragPantInicio extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragPantInicio() {
+    public FragmentInicioSes() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class FragPantInicio extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragPantPrinc.
+     * @return A new instance of fragment FragmentInicioSes.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragPantInicio newInstance(String param1, String param2) {
-        FragPantInicio fragment = new FragPantInicio();
+    public static FragmentInicioSes newInstance(String param1, String param2) {
+        FragmentInicioSes fragment = new FragmentInicioSes();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class FragPantInicio extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_pant_inicio, container, false);
+        return inflater.inflate(R.layout.fragment_inicio_ses, container, false);
     }
 }
--- CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
@@ -11,35 +11,13 @@ import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button bIni;
-    private Button bRegi;
-    private EditText usu;
-    private EditText pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bIni = findViewById(R.id.bInicio);
-        bRegi = findViewById(R.id.bRegistro);
-        usu = findViewById(R.id.usuario);
-        pass =findViewById(R.id.contrasena);
+
     }
 
-    public void pantallaRegistro(View v){
-        Intent intent = new Intent(MainActivity.this, Registro.class);
-        startActivity(intent);
-    }
-    public void inicioSesion(View v){
-        String usuario = usu.getText().toString();
-        String contrasena = pass.getText().toString();
-
-
-        if(usuario.isEmpty() || contrasena.isEmpty()){
-            Toasty.error(this, getString(R.string.errorVacioIS)).show();
-        }else{
-            Intent intentp = new Intent(MainActivity.this, pantallaPrincip.class);
-            startActivity(intentp);
-
-        }
-    }
+    
 }
