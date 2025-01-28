0b8fe2652e9030aa80fab75cfa35d2f12d63be82
machete20 <sdj0002@alu.medac.es>
2025-01-28 13:54:51 +0100

    prueba branch

--- CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.EditText;
 import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
