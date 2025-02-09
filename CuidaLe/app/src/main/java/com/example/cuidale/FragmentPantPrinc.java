63da0cc4840bee3fc3ff6e475631ea65b904b463
machete20 <sdj0002@alu.medac.es>
2025-02-09 18:35:27 +0100

    Imagenes y Fragment
    Añadidas algunas imagenes y hecho un fragment por activity y movimiento

--- CuidaLe/app/src/main/java/com/example/cuidale/FragmentPantPrinc.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/FragmentPantPrinc.java
@@ -3,16 +3,41 @@ package com.example.cuidale;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class FragmentPantPrinc extends Fragment {
+
+    private ImageView cuenta;
+    private ImageView menu;
+    private View v;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pant_princ, container, false);
+        v = inflater.inflate(R.layout.fragment_pant_princ,container,false);
+        cuenta = v.findViewById(R.id.IMGcuentaPrincp);
+        menu = v.findViewById(R.id.IMGmenuPrincp);
+
+        cuenta.setOnClickListener(v->{
+            NavController navController = Navigation.findNavController(v);
+            //navController.navigate(R.id.fragment_cuenta);
+        });
+
+        menu.setOnClickListener(v->{
+            NavController navController = Navigation.findNavController(v);
+            navController.navigate(R.id.action_fragmentPantPrinc_to_fragmentMenuDesplegable);
+        });
+
+
+        return v;
     }
 }
--- CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/MainActivity.java
@@ -1,13 +1,8 @@
 package com.example.cuidale;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import es.dmoral.toasty.Toasty;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
