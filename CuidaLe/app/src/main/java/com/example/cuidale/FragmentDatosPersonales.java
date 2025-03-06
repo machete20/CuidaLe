0ef68a70e057f94f5f82ace6138f9ac9b37107cf
machete20 <sdj0002@alu.medac.es>
2025-03-06 09:21:18 +0100

    4 retoques

--- CuidaLe/app/src/main/java/com/example/cuidale/FragmentDatosPersonales.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/FragmentDatosPersonales.java
@@ -7,13 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+public class FragmentDatosPersonales extends Fragment {
 
-public class FragmentUsuarioCuidador extends Fragment {
-    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuariocuidador, container, false);
+        return inflater.inflate(R.layout.fragment_datos_personales, container, false);
     }
 }
