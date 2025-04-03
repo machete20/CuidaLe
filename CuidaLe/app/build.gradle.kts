bda19866b7b2d68f7244041b8d439350069b66cd
machete20 <sdj0002@alu.medac.es>
2025-04-03 15:44:00 +0200

    insercion de una bd Realtime Database
    de momento solo se guarda DNI como clave principal , usuario y correo.

--- CuidaLe/app/build.gradle.kts
+++ CuidaLe/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.8.5")
     implementation("androidx.navigation:navigation-ui:2.8.5")
     implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
