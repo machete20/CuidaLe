810307eece5ef7b71da9a67eddf3163039460888
Jorge Muñoz Martínez <125618156+JorgeMMdev@users.noreply.github.com>
2025-05-04 10:19:02 +0200

    GoogleMaps
    implementacion de google maps

--- CuidaLe/app/src/main/java/com/example/cuidale/FragmentFarmarcias.java
+++ CuidaLe/app/src/main/java/com/example/cuidale/FragmentFarmarcias.java
@@ -1,5 +1,7 @@
 package com.example.cuidale;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,17 +11,23 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FragmentFarmarcias extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mymap;
+
+public class FragmentFarmarcias extends Fragment {
+
+    private View v;
+    private ImageButton atras;
+
+    private ImageButton aGoogleMaps;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +35,30 @@ public class FragmentFarmarcias extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        SupportMapFragment mapFragment = (SupportMapFragment)
-                getChildFragmentManager().findFragmentById(R.id.map);
-
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_farmarcias, container, false);
+        // Infla el layout del fragmento
+        View rootView = inflater.inflate(R.layout.fragment_farmarcias, container, false);
+
+        atras = rootView.findViewById(R.id.btn_retrocesoFarmacias);
+
+        atras.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            navController.navigate(R.id.fragmentPantPrinc);
+        });
+
+        aGoogleMaps = rootView.findViewById(R.id.previewGoogleMaps);
+
+        aGoogleMaps.setOnClickListener(v -> {
+            Uri gmmIntentUri = Uri.parse("geo:37.76922,-3.79028?q=farmacias");
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            startActivity(mapIntent);
+        });
+
+        return rootView;
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mymap = googleMap;
 
-        LatLng jaen =  new LatLng(37.76922, -3.79028);
-        mymap.addMarker(new MarkerOptions().position(jaen).title("Jaén"));
-        mymap.moveCamera(CameraUpdateFactory.newLatLngZoom(jaen, 12));
-    }
 }
